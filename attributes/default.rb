6eebfa276e287a80753e645258ccb6467fccef6f
Anthony Carminati <anthony@carminati.io>
2016-02-05 19:46:22 -0600

    added config files for production release, updated .gitignore, added conf info to attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,3 +5,5 @@
 # Copyright (c) 2016 Anthony Carminati, All Rights Reserved.
 
 default[:chef_app_analytics_portal][:app_dir] = '/opt/app_dir'
+default[:chef_app_analytics_portal][:pip_requirements] = '/opt/requirements.txt'
+default[:chef_app_analytics_portal][:nginx_conf] = '/etc/nginx/nginx.conf'
--- recipes/configure.rb
+++ recipes/configure.rb
@@ -1,5 +1,5 @@
 #
-# Cookbook Name:: chef_env_analytics
+# Cookbook Name:: chef_app_analytics_portal
 # Recipe:: configure
 #
 # Copyright (c) 2016 Anthony Carminati, All Rights Reserved.
@@ -9,6 +9,17 @@
 ## ----------
 service "codedeploy-agent" do
   action [:enable, :start]
+end
+
+## ----------
+## Copy python-pip requirements file to :pip_requirements
+## ----------
+cookbook_file node[:chef_app_analytics_portal][:pip_requirements] do
+  source 'requirements.txt'
+  owner 'root'
+  group 'root'
+  mode '755'
+  action :create
 end
 
 ## ----------
@@ -29,9 +40,15 @@ node[:pip_python_packages].each_pair do |pkg, version|
   end
 end
 
-# node[:pip_python_packages].each do |pkg|
-#     execute "install-#{pkg}" do
-#         command "pip install #{pkg}"
-# #        not_if "[ `pip freeze | grep #{pkg} | cut -d'=' -f3` = '#{version}' ]"
-#     end
-# end
+python
+
+## ----------
+## Copy nginx config file to :nginx_conf
+## ----------
+cookbook_file node[:chef_app_analytics_portal][:nginx_conf] do
+  source 'nginx.conf'
+  owner 'root'
+  group 'root'
+  mode '755'
+  action :create
+end
